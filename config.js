e5249b6efed487148de01128aa9e3d5f73ded478
Muhammad Ali <muhammad.p.ali@outlook.com>
2024-07-23 23:32:34 -0400

    project7byMAli folder commit

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    API_URL: "https://cnt-a0fd31b7-5cc8-48ac-b2bd-a40cda10da81.containerhub.tripleten-services.com"
+    API_URL: "https://cnt-32ede139-2334-453b-a4a8-1887536f1e8e.containerhub.tripleten-services.com"
 }
